83bf0e094682ac2167991ff28dc627a651dc8e8d
Mohamed-lehbib <abeidnamohamedlehbib@gmail.com>
2023-07-23 22:16:45 +0000

    adding gitignore and .env

--- stationProject/settings.py
+++ stationProject/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')(%@q_!!%&=#b9d5whug&gc*&rv!@k6l_z0ymc+1)#ti^!6kfq'
+from dotenv import load_dotenv
+load_dotenv()
+
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
